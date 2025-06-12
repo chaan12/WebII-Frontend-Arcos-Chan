5827ea81f85e90296e47bf08a63fc11784089e03
chaan12 <eduardoezequielchanortiz@gmail.com>
2025-06-12 13:16:28 -0600

    Responsive

--- sistema-pago-colegiaturas/src/components/Sidebar.jsx
+++ sistema-pago-colegiaturas/src/components/Sidebar.jsx
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/components/Sidebar.css";
 
 export default function Sidebar() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
   return (
-    <aside className="sidebar">
-      <div
-        className="sidebar-header"
-      >
-        <h2>Alumno</h2>
-      </div>
+    <>
+      <aside className={`sidebar ${open ? "open" : ""}`}>
+        <button className="burger" onClick={() => setOpen(!open)}>
+          ☰
+        </button>
+        <div className="sidebar-header">
+          <h2>Alumno</h2>
+        </div>
 
-      <ul className="sidebar-menu">
-        <li onClick={() => navigate("/libreta-pago")}>Libreta de Pago</li>
-        <li onClick={() => navigate("/realizar-pago")}>Realizar Pago</li>
-        <li onClick={() => navigate("/mi-cuenta")}>Mi Cuenta</li>
-        <li
-          onClick={() => {
-            localStorage.clear();
-            navigate("/alumnos");
-          }}
-        >
-          Cerrar Sesión
-        </li>
-      </ul>
-    </aside>
+        <ul className="sidebar-menu">
+          <li onClick={() => navigate("/libreta-pago")}>Libreta de Pago</li>
+          <li onClick={() => navigate("/realizar-pago")}>Realizar Pago</li>
+          <li onClick={() => navigate("/mi-cuenta")}>Mi Cuenta</li>
+          <li
+            onClick={() => {
+              localStorage.clear();
+              navigate("/alumnos");
+            }}
+          >
+            Cerrar Sesión
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 }
